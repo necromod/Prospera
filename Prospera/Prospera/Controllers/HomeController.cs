b176e04fdabc980686e1047be3e30605886542a4
Warlock <edimilson.ribeiro019@gmail.com>
2023-10-23 20:35:39 -0300

    Adição de páginas inicias cshtml

--- Prospera/Prospera/Controllers/HomeController.cs
+++ Prospera/Prospera/Controllers/HomeController.cs
@@ -22,6 +22,10 @@ namespace Prospera.Controllers
         {
             return View();
         }
+        public IActionResult MenuUsuario()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
