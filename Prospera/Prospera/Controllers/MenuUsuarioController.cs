bd846eb58a8712a16b8c1eb2d4742048a9d303cf
warlock <edimilson.ribeiro019@gmail.com>
2023-11-06 17:52:53 -0300

    Finalizado exclusão de Terceiros

--- Prospera/Prospera/Controllers/MenuUsuarioController.cs
+++ Prospera/Prospera/Controllers/MenuUsuarioController.cs
@@ -35,35 +35,103 @@ namespace Prospera.Controllers
 
         // POST: Criação de campo Terceiros
         [HttpPost]
-        public IActionResult CadastrarTerceiro(TerceirosViewModel terceiros)
+        public IActionResult CadastrarTerceiro(TerceirosViewModel terceiros, string btnAcao)
         {
-
-            //Terceiros testeExterno = new Terceiros();
-            //testeExterno = _teerceirosViewModel.NovoTerceiro();
-
-            //Verifica se o usuário está logado
-            if (_sessao.BuscarSessaoUsuario() != null)
+            if (btnAcao == "Consultar")
             {
-                Usuario usuarioModel = _sessao.BuscarSessaoUsuario();
-                terceiros.NovoTerceiro.IdUsuario = usuarioModel.IdUsuario;
+                if (int.TryParse(terceiros.NovoTerceiro.IdTerceiros.ToString(), out int id))
+                {
+                    var terceiro = _context.Terceiros.FirstOrDefault(t => t.IdTerceiros == id);
+
+                    if (terceiro != null)
+                    {
+                        return View();
+                        /*// Retorne os valores encontrados em formato JSON
+                        return Json(new
+                        {
+                            IdTerceiros = terceiro.IdTerceiros,
+                            NomeTerceiros = terceiro.NomeTerceiros,
+                            EmailTerceiros = terceiro.EmailTerceiros,
+                            TelefoneTerceiros = terceiro.TelefoneTerceiros,
+                            // Adicione outros campos conforme necessário
+                        });*/
+                    }
+                }
             }
-            else
+
+
+
+
+
+            //botão de Cadastrar foi pressionado
+            if (btnAcao == "cadastro")
             {
-                terceiros.NovoTerceiro.IdUsuario = 55;
+
+                //Terceiros testeExterno = new Terceiros();
+                //testeExterno = _teerceirosViewModel.NovoTerceiro();
+
+                //Verifica se o usuário está logado
+                if (_sessao.BuscarSessaoUsuario() != null)
+                {
+                    Usuario usuarioModel = _sessao.BuscarSessaoUsuario();
+                    terceiros.NovoTerceiro.IdUsuario = usuarioModel.IdUsuario;
+                }
+                else
+                {
+                    terceiros.NovoTerceiro.IdUsuario = 55;
+                }
+
+
+
+                //inserção automática dos campos
+                terceiros.NovoTerceiro.IdTerceiros = 0;
+                terceiros.NovoTerceiro.DataCadastroTerceiros = DateTime.Now;
+                terceiros.NovoTerceiro.DataUltimaMovimentacao = DateTime.Now;
+                terceiros.NovoTerceiro.StatusTerceiros = "Ativo";
+
+                //Criação do campo dentro do banco de dados
+                _context.Terceiros.Add(terceiros.NovoTerceiro);
+                _context.SaveChanges();
             }
 
-            
+            //botão de exclusao foi pressionado
+            else if (btnAcao == "exclusao")
+            {
+                // Certifique-se de que o ID seja uma string válida.
+                if (int.TryParse(terceiros.NovoTerceiro.IdTerceiros.ToString(), out int id))
+                {
+                    // Verifique se o ID existe no banco de dados.
+                    var terceiro = _context.Terceiros.FirstOrDefault(t => t.IdTerceiros == id);
 
-            //inserção automática dos campos
-            terceiros.NovoTerceiro.DataCadastroTerceiros = DateTime.Now;
-            terceiros.NovoTerceiro.DataUltimaMovimentacao = DateTime.Now;
-            terceiros.NovoTerceiro.StatusTerceiros = "Ativo";
+                    if (terceiro != null)
+                    {
+                        // O ID existe no banco de dados, você pode excluí-lo.
+                        _context.Terceiros.Remove(terceiro);
+                        _context.SaveChanges();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Não funcionou IF terceiro != null");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("entrada do usuário não é um número inteiro válido");
+                    // A entrada do usuário não é um número inteiro válido, adote a ação apropriada.
+                    // Você pode retornar uma mensagem de erro ou redirecionar para uma página apropriada.
+                }
+            }
+            //botão de Alterar foi pressionado
+            else if (btnAcao == "Alterar")
+            {
 
-            //Criação do campo dentro do banco de dados
-            _context.Terceiros.Add(terceiros.NovoTerceiro);
-            _context.SaveChanges();
+            }
 
-            return RedirectToAction("MenuUsuario", "MenuUsuario");
+            return Json(null);
+            //return RedirectToAction("MenuUsuario", "MenuUsuario");
+
         }
+
+
     }
 }
