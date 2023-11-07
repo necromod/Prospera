1db384548ba34efb65ef855b608040cf8fd9fd89
warlock <edimilson.ribeiro019@gmail.com>
2023-11-07 03:16:49 -0300

    Configurado consulta Terceiros

---
+++ Prospera/Prospera/wwwroot/js/Buscar.js
@@ -0,0 +1,42 @@
+﻿$(document).ready(function () {
+    $("#BtnTerceirosBuscar").click(function () {
+        buscarTerceiros();
+    });
+});
+
+function buscarTerceiros() {
+    var idTerceiros = $("#TxtTerceirosId").val();
+
+    $.ajax({
+        url: "/MenuUsuario/BuscarTerceiros",
+        method: "GET",
+        data: { id: idTerceiros },
+        success: function (data) {
+            if (data) {
+                preencherCampos(data);
+                console.log("asdasdasd.");
+                console.log(data);
+            } else {
+                console.log("Terceiros não encontrado no banco de dados.");
+            }
+        },
+        error: function () {
+            alert("Erro ao buscar Terceiros.");
+        }
+    });
+}
+
+function preencherCampos(terceiros) {
+    $("#TxtTerceirosId").val(terceiros.idTerceiros);
+    $("#TxtTerceirosNome").val(terceiros.nomeTerceiros);
+    $("#TxtTerceirosEmail").val(terceiros.emailTerceiros);
+    $("#TxtTerceirosTelefone").val(terceiros.telefoneTerceiros);
+    $("#TxtTerceirosTelefone2").val(terceiros.telefone2Terceiros);
+    $("#TxtTerceirosEndereco").val(terceiros.enderecoTerceiros);
+    $("#TxtTerceirosCidade").val(terceiros.cidadeTerceiros);
+    $("#TxtTerceirosBairro").val(terceiros.bairroTerceiros);
+    $("#TxtTerceirosUF").val(terceiros.ufTerceiros);
+    $("#TxtTerceirosCEP").val(terceiros.cepTerceiros);
+    $("#TxtTerceirosObservacao").val(terceiros.observacaoTerceiros);
+}
+
