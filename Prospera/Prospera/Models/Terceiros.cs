c2d424e993a88a1358dd6b333190b6eac3e9d427
warlock <edimilson.ribeiro019@gmail.com>
2023-11-07 13:31:52 -0300

    Ajuste validações Terceiros

--- Prospera/Prospera/Models/Terceiros.cs
+++ Prospera/Prospera/Models/Terceiros.cs
@@ -7,41 +7,51 @@ namespace Prospera.Models
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int IdTerceiros { get; set; }
+        public int? IdTerceiros { get; set; }
 
-        [Required]
+        [Required(ErrorMessage ="Campo obrigatório!")]
         [StringLength(120)]
         public string NomeTerceiros { get; set; }
 
         [StringLength(20)]
+        [Required(ErrorMessage = "Campo obrigatório!")]
+
         public string TelefoneTerceiros { get; set; }
 
         [StringLength(20)]
-        public string Telefone2Terceiros { get; set; }
+        public string? Telefone2Terceiros { get; set; }
 
         [StringLength(120)]
-        [EmailAddress]
+        [EmailAddress(ErrorMessage = "E-mail inválido.")]
+        [Required(ErrorMessage = "Campo obrigatório!")]
+
         public string EmailTerceiros { get; set; }
 
         [StringLength(80)]
+        [Required(ErrorMessage = "Campo obrigatório!")]
         public string EnderecoTerceiros { get; set; }
 
         [StringLength(80)]
+        [Required(ErrorMessage = "Campo obrigatório!")]
         public string CidadeTerceiros { get; set; }
 
         [StringLength(80)]
+        [Required(ErrorMessage = "Campo obrigatório!")]
         public string BairroTerceiros { get; set; }
 
         [StringLength(10)]
+        [Required(ErrorMessage = "Campo obrigatório!")]
         public string UFTerceiros { get; set; }
 
         [StringLength(10)]
+        [Required(ErrorMessage = "Campo obrigatório!")]
         public string CEPTerceiros { get; set; }
 
         [StringLength(80)]
-        public string ObservacaoTerceiros { get; set; }
+        public string? ObservacaoTerceiros { get; set; }
 
         [DataType(DataType.Date)]
+
         public DateTime DataCadastroTerceiros { get; set; }
 
         [DataType(DataType.Date)]
